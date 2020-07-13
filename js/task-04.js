8195d562d22de37bf778c954e086873084d1fea9
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-13 20:28:04 +0300

    Add task 04

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,15 @@
 "use strict";
+const formatString = function (string) {
+  if (string.length >= 40) {
+    string = string.slice(0, 39) + "...";
+  }
+  return string;
+};
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+console.log(formatString("Curabitur ligula sapien."));
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
