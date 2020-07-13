3ce1e0624b08f84b7981a16060a38919c68f45f7
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-13 19:11:14 +0300

    add task 02

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// const findLongestWord = function (string) {
+//   const inputString = string.split(" ");
+//   let maxLettersWord = 0;
+//   for (let i = 0; i < inputString.length; i += 1) {
+//     if (inputString[i].length > maxLettersWord) {
+//       maxLettersWord = i;
+//     }
+//   }
+//   //   return maxLettersWord;
+//   return inputString[maxLettersWord];
+// };
+const findLongestWord = function (string) {
+  const inputString = string.split(" ");
+  let inputStringLength = 0;
+  let maxLettersWord = 0;
+  for (let i = 0; i < inputString.length; i += 1) {
+    if (inputString[i].length > inputStringLength) {
+      inputStringLength = inputString[i].length;
+      maxLettersWord = i;
+    }
+  }
+  return inputString[maxLettersWord];
+};
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+
+console.log(findLongestWord("Google do a roll"));
+
+console.log(findLongestWord("May the force be with you"));
