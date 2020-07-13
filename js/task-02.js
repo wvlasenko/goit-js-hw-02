489628e2f88793dea3c1700bf73527ad931525ed
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-13 12:43:48 +0300

    Add task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,24 @@
 "use strict";
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const wordsForEngraving = message.split(" ");
+  const finalCoast = wordsForEngraving.length * pricePerWord;
+  return finalCoast;
+};
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+);
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+);
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+);
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+);
