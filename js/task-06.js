b36acb59f12729ca30e6dde8e8334e00dab06998
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-14 16:42:30 +0300

    Add task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,35 @@
 "use strict";
+let input;
+let userInput;
+const numbers = [];
+let total = 0;
+do {
+  input = prompt("Введите число : ");
+  userInput = input;
+  input = Number(input);
+  const notANumber = Number.isNaN(input);
+  if (notANumber) {
+    alert("Было введено не число, попробуйте еще раз");
+    continue;
+  } else if (userInput === "") {
+    input = "";
+    alert("Вы ничего не ввели");
+    continue;
+  } else if (userInput === null) {
+    break;
+  } else {
+    numbers.push(input);
+  }
+  // console.log(input);
+} while (true);
+if (numbers.length === 0) {
+  /* При загрузке страници пользователь сразу нажал отмену*/
+  alert("Подсчет невозможен - Вы ничего не ввели");
+} else {
+  for (const number of numbers) {
+    // console.log(number);
+    total += number;
+    // console.log(total);
+  }
+  alert(`Сумма введенных чисел = ${total}`);
+}
