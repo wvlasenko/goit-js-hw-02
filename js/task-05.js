c3dab2c8bae66d2c14189ac11a8e7d26148228cd
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-14 15:18:10 +0300

    add task 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,20 @@
 "use strict";
+let resultMessage = "falce";
+const firstNameToFind = "spam";
+const secondNameToFind = "sale";
+const checkForSpam = function (message) {
+  let normalizeMassage = message.toLowerCase();
+  const inputWords = normalizeMassage.split(" ");
+  // console.log(inputWords);
+  for (const inputWord of inputWords) {
+    if (firstNameToFind === inputWord || secondNameToFind === inputWord) {
+      resultMessage = "true";
+      break;
+    }
+  }
+  return resultMessage;
+};
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
